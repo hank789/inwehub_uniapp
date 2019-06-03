d65be3242505937ddc31d6c20fee5a32d9260d11
hank <hank.huiwang@gmail.com>
2019-06-03 17:09:38 +0800

    我的设置

--- pages/my/setting.vue
+++ pages/my/setting.vue
@@ -1,13 +1,12 @@
 <template>
 	<view class="">
-		<cmd-item title="绑定手机" @click="fnClick('modify')" addon="18300624364" arrow></cmd-item>
-		<cmd-item title="绑定微信" @click="fnClick('modify')" addon="18300624364"></cmd-item>
-		<cmd-item title="修改密码" @click="fnClick('modify')" addon="" arrow></cmd-item>
-		<cmd-item title="推送设置" @click="fnClick('modify')" addon="" arrow></cmd-item>
-		<cmd-item title="常见问题" @click="fnClick('modify')" addon="" arrow></cmd-item>
-		<cmd-item title="关于我们" @click="fnClick('modify')" addon="" arrow></cmd-item>
-		<cmd-item title="前往评价" @click="fnClick('modify')" addon="" arrow></cmd-item>
-		<cmd-item title="清楚缓存" @click="fnClick('modify')" addon="" arrow></cmd-item>
+		<cmd-item title="绑定手机" @click="fnClick('modify')" :addon="phone" arrow></cmd-item>
+		<cmd-item title="绑定微信" @click="fnClick('bindWechat')" :addon="bindWeixinNickname"></cmd-item>
+		<cmd-item title="推送设置" @click="fnClick('pushSetting')" addon="" arrow></cmd-item>
+		<cmd-item title="常见问题" @click="fnClick('')" addon="" arrow></cmd-item>
+		<cmd-item title="关于我们" @click="fnClick('aboutus')" addon="" arrow></cmd-item>
+		<cmd-item title="前往评价" @click="fnClick('starApp')" addon="" arrow></cmd-item>
+		<cmd-item title="清楚缓存" @click="fnClick('clearCache')" addon="" arrow></cmd-item>
 		<button type="button" class="btn-logout" @tap.stop.prevent="logOut">退出应用</button>
 		<view class="foot">
 			<view class="logo">
@@ -20,28 +19,111 @@
 				<view class="copyrightDesc">All Rights Reserved</view>
 			</view>
 		</view>
+		<oauth ref="oauth" serviceId="weixin" :isShowBtn="false" @success="wechatLoginSuccess" @fail="wechatLoginFail"></oauth>
 	</view>
 </template>
 
 <script>
 	import cmdItem from "@/components/cmd-item/cmd-item.vue"
+	import oauth from '@/components/oauth/oauth.vue'
+
 	export default {
 		components: {
-			cmdItem
+			cmdItem,
+			oauth
 		},
 
 		data() {
-			return {};
+			return {
+				bindWeixinNickname: '', // 绑定微信名称
+        isBindWeixin: 0, // 是否绑定微信
+        appVersion: '',
+        im_tokenMsg: '',
+        name: '',
+        phone: '',
+        avatar: '',
+        title: '',
+        company: '',
+        ios_market_url: '',
+        android_market_url: ''
+			};
 		},
-		mounted() {},
+		onLoad() {
+			//#ifdef APP-PLUS
+			var version = this.$ls.get('app_install_version')
+			var appDeviceInfo = this.$ls.get('appDeviceInfo')
+      if (version) {
+        if (appDeviceInfo.platform == 'ios') {
+          this.appVersion = 'for iPhone V' + version
+        } else {
+          this.appVersion = 'for android V' + version
+        }
+      }
+			//#endif
+			var userInfo = this.$ls.get('UserInfo')
+			this.name = userInfo.info.name
+			this.phone = userInfo.info.mobile
+			this.avatar = userInfo.info.avatar_url
+			this.title = userInfo.info.title
+			this.company = userInfo.info.company
+			this.getWallet()
+		},
 		methods: {
 			fnClick (type) {
 				switch (type){
 					case 'modify':
 						uni.navigateTo({ url: '/pages/wechat/bindPhone' })
 						break;
+					case 'pushSetting':
+						uni.navigateTo({ url: '/pages/inform/setting/pushSetting' })
+						break;
+					case 'clearCache':
+						this.clearUserCache()
+						break;
+					case 'bindWechat':
+						if (!this.isBindWeixin) {
+							this.$refs.oauth.login()
+						}
+						break;
+					case 'aboutus':
+						uni.navigateTo({ url: '/pages/protocol/aboutus' })
+						break;
+					case 'starApp':
+						var appDeviceInfo = this.$ls.get('appDeviceInfo')
+						this.$request.get(`system/app_market_url`, {}).then(responseData => {
+							if (responseData.data !== false) {
+								this.ios_market_url = responseData.data.ios_url
+								this.android_market_url = responseData.data.android_url
+								if (appDeviceInfo.platform == 'ios') {
+									plus.runtime.openURL(this.ios_market_url)
+								} else if (appDeviceInfo.platform == 'android') {
+									plus.runtime.openURL(this.android_market_url, function (e) {
+										uni.showToast({
+											title: '很抱歉，您未安装腾讯应用宝，暂时无法评分，感谢支持'
+										})
+									}, 'com.tencent.android.qqdownloader')
+								} else {
+									window.location.href = this.ios_market_url
+								}
+							}
+						})
+						break;
 				}
 			},
+			getWallet () {
+        this.$request.post(`account/wallet`, {}).then(response => {
+          var code = response.code
+          if (code !== 1000) {
+            return
+          }
+          var data = response.data
+          this.isBindWeixin = data.is_bind_weixin
+          this.bindWeixinNickname = data.bind_weixin_nickname
+					if (!this.bindWeixinNickname) {
+						this.bindWeixinNickname = '前往绑定'
+					}
+        })
+      },
 			clearUserCache() {
 				this.$ls.remove('UserLoginInfo')
 				this.$ls.remove('UserInfo')
@@ -65,7 +147,39 @@
 				})
 				//#endif
 				this.clearUserCache()
-			}
+			},
+			getOperators () {
+        //#ifdef APP-PLUS
+				this.$request.get(`system/getOperators`, {}, false).then(responseData => {
+					if (responseData.data.length > 0) {
+						var btnArray = []
+						responseData.data.forEach(function (i, r) {
+							btnArray.push({title: i.name, id: i.id})
+						})
+						plus.nativeUI.actionSheet({
+							title: '切换账户',
+							cancel: '取消',
+							buttons: btnArray
+						}, (e) => {
+							if (e.index > 0) {
+								this.$request.get('auth/operatorLogin', {
+									user_id: btnArray[e.index - 1].id
+								})
+									.then(response => {
+										if (response.data === false) {
+											return
+										}
+										this.$ls.set('token', response.data.token)
+										uni.switchTab({
+											url: '/pages/my/index'
+										})
+									})
+							}
+						})
+					}
+				})
+        //#endif
+      }
 		}
 	}
 </script>
