9a249de81092ffb613436630351509cf1599a4a7
edwin <edwincoder@163.com>
2019-06-04 17:40:19 +0800

    add 社区页

---
+++ pages/dianping/products.vue
@@ -0,0 +1,37 @@
+<template>
+    <view>
+
+    </view>
+</template>
+
+<script>
+
+  export default {
+    data(){
+      return {
+        loading: 1
+      }
+    },
+    computed: {
+      nothing () {
+        return false;
+      },
+    },
+    methods: {
+      nothing(){
+
+      }
+    },
+    mounted(){
+
+    },
+
+    created(){
+
+    }
+  };
+</script>
+
+<style lang="less" rel="stylesheet/less" scoped>
+
+</style>
