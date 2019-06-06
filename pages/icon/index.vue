6c14ceacf34f0817a09b9dbacd0b781c733eac1d
edwin <edwincoder@163.com>
2019-06-06 16:39:38 +0800

    add icon preview

---
+++ pages/icon/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <view class="content">
+        <text class="iconfont icon-shouye"></text>
+        <text class="iconfont icon-shouye disable"></text>
+
+        <text class="iconfont icon-guanzhudilan"></text>
+        <text class="iconfont icon-guanzhudilan disable"></text>
+
+        <text class="iconfont icon-shequ1"></text>
+        <text class="iconfont icon-shequ1 disable"></text>
+
+        <text class="iconfont icon-wode2"></text>
+        <text class="iconfont icon-wode2 disable"></text>
+    </view>
+</template>
+
+<style>
+    page{
+        background: transparent;
+    }
+    .content{
+        padding:30px;
+        background: transparent;
+    }
+    .iconfont{
+        font-size: 81px;
+        color:#444;
+    }
+    .disable{
+        color:#B4B4B6;
+    }
+</style>
