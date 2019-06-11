7fb8908d6a787e81cf5abf88558c0c07c728b8cf
hank <hank.huiwang@gmail.com>
2019-06-11 18:10:49 +0800

    我的关注&优化个人简历（查看其他人简历）

---
+++ components/user-directory/list.vue
@@ -0,0 +1,174 @@
+<template>
+	<view>
+		<scroll-view class="scroll-list"
+		:scroll-top="1"
+		scroll-y="true"
+		:scroll-with-animation="scrollAnimationOFF" 
+		:scroll-into-view="scrollViewId" 
+		:style="{height:winHeight + 'px', width: winWidth + 'px'}" 
+		@scroll="handleScroll">
+			<view class="phone-list">
+				<view class="list-item" 
+				v-for="(item, key) of phones" 
+				:key="key" 
+				:id="key">
+					<view class="list-item-title">{{key}}</view>
+					<view class="list-item-phone" 
+					@click="handleClick(innerItem)"
+					hover-class="commonly-hover" 
+					:hover-start-time="20" 
+					:hover-stay-time="70" 
+					v-for="innerItem in item"
+					:key="innerItem.id"
+					>
+					<view class="uni-media-list">
+						<image class="uni-media-list-logo" style="border-radius: 50%;" :src="innerItem.user_avatar_url"></image>
+						<view class="uni-media-list-body">
+							<view class="uni-media-list-text-top">{{innerItem.user_name}}</view>
+							<view class="uni-media-list-text-bottom">
+								<view><text class="uni-article-title">{{innerItem.description}}</text></view>
+							</view>
+						</view>
+					</view>
+					</view>
+				</view>
+			</view>
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"phone-list",
+		props:{
+			phones:Object,
+			letter:String,
+			scrollAnimationOFF:Boolean
+		},
+		data () {
+			return {
+				winHeight:0,
+				winWidth: 0,
+				scrollTop:0,
+				letterDetails:[],
+				timer:null
+			}
+		},
+		computed:{
+			scrollViewId () {
+				return this.letter
+			}
+		},
+		mounted(){
+			let windowWidth = uni.getSystemInfoSync().windowWidth
+			this.winWidth = windowWidth - 30
+			// #ifndef APP-PLUS
+			this.winHeight = uni.getSystemInfoSync().windowHeight - 49.50
+			//#endif
+			
+			//#ifdef APP-PLUS
+			this.winHeight = uni.getSystemInfoSync().windowHeight - 100
+			//#endif
+
+		},
+		methods:{
+			handleClick (e) {
+				this.$emit('handleClick',e)
+			},
+			followUser(id, key, index) {
+				this.$request.post('follow/user',{id: id}).then(res => {
+					var code = res.code
+					if (code !== 1000) {
+						uni.showToast({
+							title:res.message
+						})
+						return
+					}
+					this.list[index].is_following = !this.list[index].is_following
+					uni.showToast({
+						title: res.data.tip
+					})
+				})
+			},
+			handleScroll (e){
+				if(this.letterDetails.length === 0){
+					let view = uni.createSelectorQuery().selectAll('.list-item')
+					view.boundingClientRect(data=>{
+						let top = data[0].top
+						data.forEach((item,index)=>{
+							item.top = item.top - top
+							item.bottom  = item.bottom - top
+							this.letterDetails.push({
+								id:item.id,
+								top:item.top,
+								bottom:item.bottom
+							})
+						})
+					}).exec()	
+				}
+				
+				const scrollTop = e.detail.scrollTop
+				this.letterDetails.some((item,index)=>{
+					if(scrollTop>=item.top && scrollTop <= item.bottom - 5){
+						this.$emit('change',item.id)
+						this.$emit('reset',true)
+						return true
+					}
+				})
+			}
+		}
+			
+	}
+</script>
+
+<style>
+	
+	.commonly-hover{
+		background-color: #eee;
+	}
+	
+	.scroll-list{
+		flex: 1;
+		height: 100vh;
+		overflow-y: hidden;
+	}
+
+	.phone-list{
+		display: flex;
+		background-color: #fff;
+		flex-direction:column;
+		position:relative;
+		width: 100%;
+	}
+	
+	.list-item {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		flex-wrap:wrap;
+		height: 92upx;
+		background-color: #fff;
+		height: 100%;
+		
+	}
+	
+	.list-item >.list-item-phone{
+		font-weight: normal;
+	}
+	
+	.list-item-title{
+		background-color: #eee;
+	}
+	
+	.list-item-title,.list-item-phone{
+		width: 100%;
+		line-height: 92upx;
+		font-size: 32upx;
+		font-weight: bold;
+		padding: 0 20upx;
+		border-bottom: 1px solid #e5e5e5;
+	}
+	
+	
+
+</style>
---
+++ pages/my/addressBook.vue
@@ -0,0 +1,154 @@
+<template>
+	<view class="">
+		
+		<view class="mui-content">
+
+      <view class="contactusWrapper">
+        <view class="title font-family-medium">联系我们</view>
+        <view class="textWrapper">
+          <view class="text textBottom">
+            <text>客服微信：hiinwe</text>
+            <text class="textColor" @tap.stop.prevent="showWeChat">添加</text>
+          </view>
+          <view class="text">
+            <text>联系邮箱：hi@inwehub.com</text>
+            <text class="textColor" @click="setClipboardData('hi@inwehub.com')">复制</text>
+          </view>
+        </view>
+        <view class="bot"></view>
+      </view>
+
+      <view class="contactusWrapper">
+        <view class="title font-family-medium">广告合作</view>
+        <view class="textWrapper">
+          <view class="text">
+            <text>联系邮箱：Ad@inwehub.com</text>
+            <text class="textColor" @click="setClipboardData('Ad@inwehub.com')">复制</text>
+          </view>
+        </view>
+        <view class="bot"></view>
+      </view>
+
+      <view class="contactusWrapper">
+        <view class="title font-family-medium">成为合作伙伴</view>
+        <view class="textWrapper">
+          <view class="text">
+            <text>联系邮箱：BD@inwehub.com</text>
+            <text class="textColor" @click="setClipboardData('BD@inwehub.com')">复制</text>
+          </view>
+        </view>
+        <view class="bot"></view>
+      </view>
+
+      <view class="contactusWrapper">
+        <view class="title font-family-medium">投稿/寻求报道</view>
+        <view class="textWrapper">
+          <view class="text">
+            <text>联系邮箱：Hi@inwehub.com</text>
+            <text class="textColor" @click="setClipboardData('Hi@inwehub.com')">复制</text>
+          </view>
+        </view>
+        <view class="bot"></view>
+      </view>
+
+      <view class="contactusWrapper">
+        <view class="title font-family-medium">加入我们</view>
+        <view class="textWrapper">
+          <view class="text">
+            <text>联系邮箱：HR@inwehub.com</text>
+            <text class="textColor" @click="setClipboardData('HR@inwehub.com')">复制</text>
+          </view>
+        </view>
+        <!--<view class="bot"></view>-->
+      </view>
+    </view>
+		<add-inwehub-dialog ref="addInwehubDialog" :showPopup="showPopup" @clickButton="clickDialogButton"></add-inwehub-dialog>
+	</view>
+</template>
+
+<script>
+	import addInwehubDialog from "@/components/iw-dialog/add-inwehub.vue"
+	export default {
+		data() {
+			return {
+				showPopup: false
+			}
+		},
+		components: {
+			addInwehubDialog
+		},
+		methods: {
+			clickDialogButton() {
+				this.$refs.addInwehubDialog.hidePopup()
+			},
+			showWeChat() {
+				this.$refs.addInwehubDialog.showDialog()
+			},
+			setClipboardData(msg) {
+				uni.setClipboardData({
+					data: msg,
+					success: function () {
+							uni.showToast({
+								title: '复制成功',
+								icon: 'success'
+							})
+					}
+				});
+			}
+		},
+	}
+</script>
+
+<style lang="less">
+	
+	.mui-content {
+    background: #ffffff;
+  }
+  .bot {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0upx;
+    height: 1.96upx;
+    -webkit-transform: scaleY(.5);
+    transform: scaleY(.5);
+    background-color: rgb(220, 220, 220);
+  }
+
+
+  .contactusWrapper {
+    position: relative;
+    padding: 0 31.96upx;
+    margin-top: 33.98upx;
+    .bot {
+      right: 36upx;
+      left: 31.96upx;
+    }
+    .title {
+      color: #444444;
+      font-size: 30upx;
+      line-height: 42upx;
+    }
+    .textWrapper {
+      padding-bottom: 31.96upx;
+      .textBottom {
+        margin-bottom: 27.98upx !important;
+      }
+      .text {
+        display: flex;
+        justify-content: space-between;
+        color: #808080;
+        font-size: 27.98upx;
+        line-height: 39.98upx;
+        &:nth-of-type(1) {
+          margin: 18upx 0 0upx;
+        }
+		.textColor {
+			color: #03AEF9;
+		}
+      }
+    }
+
+  }
+	
+</style>
