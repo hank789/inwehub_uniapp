a75ca573a3b6c36e3a9efd8e1d2fb44e5f8ee018
hank <hank.huiwang@gmail.com>
2019-06-19 19:51:13 +0800

    首页使用nvue实现

--- config/index.js
+++ config/index.js
@@ -1,13 +1,18 @@
 	import store from '../store'
 	import devEnv from './dev.env.js'
 	import prodEnv from './prod.env.js'
+	import ls from '@/lib/localstorage.js'
 	
 	if (process.env.NODE_ENV === 'development') {
 		store.commit('setBaseApiUrl', devEnv.BASE_API_URL)
     store.commit('setWebRoot', devEnv.H5_ROOT)
+		ls.set('baseApiUrl',devEnv.BASE_API_URL)
+		ls.set('webRoot',devEnv.H5_ROOT)
 		console.log('开发环境:' + devEnv.BASE_API_URL)
 	} else {
 		store.commit('setBaseApiUrl', prodEnv.BASE_API_URL)
     store.commit('setWebRoot', prodEnv.H5_ROOT)
+		ls.set('baseApiUrl',prodEnv.BASE_API_URL)
+		ls.set('webRoot',prodEnv.H5_ROOT)
 		console.log('生产环境')
 	}
