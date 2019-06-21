7cef0b36b3c570b75a5fdbe377aa111d1842082e
hank <hank.huiwang@gmail.com>
2019-06-21 21:36:44 +0800

    文章详情评论

--- pages/webview/article.vue
+++ pages/webview/article.vue
@@ -45,25 +45,18 @@
 						progress: {
 							color: '#FF3333'
 						},
-						bottom: '0px',
-						subNViews:[
-							{
-								id:'subnview1',
-								styles:{height:'44px',backgroundColor:'#FFFFFF',dock: 'bottom',position: 'dock'},
-								tags:[
-									{tag:'input',id:'input',position:{top: navBarTop + 'px',height: '44px'},inputStyles:{placeholder: '点击评论',borderColor: '#FF3333',borderRadius: '6px',fonstSize:'16px'}}
-								]
-							}
-						],
+						bottom: '44px',
             plusrequire:"none", //禁止远程网页使用plus的API，有些使用mui制作的网页可能会监听plus.key，造成关闭页面混乱，可以通过这种方式禁止
             top:uni.getSystemInfoSync().statusBarHeight+44 //放置在titleNView下方。如果还想在webview上方加个地址栏的什么的，可以继续降低TOP值
         })
         wv.loadURL(this.detailData.url)
         var currentWebview = this.$mp.page.$getAppWebview() //获取当前页面的webview对象
         currentWebview.append(wv);//一定要append到当前的页面里！！！才能跟随当前页面一起做动画，一起关闭
-        setTimeout(function() {
-            console.log(wv.getStyle())
-        }, 1000);//如果是首页的onload调用时需要延时一下，二级页面无需延时，可直接获取
+        let subNVue = uni.getSubNVueById('comment_footer')
+				
+				subNVue.show('fade-in', 200, () => {
+					
+				})
         // #endif
     },
 		methods: {
