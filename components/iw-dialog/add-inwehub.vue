dc2ccc1ea4fa3c302a38286554f98c18582be78b
陈灵灵 <798092775@qq.com>
2019-07-01 14:28:11 +0800

    添加小哈微信

--- components/iw-dialog/add-inwehub.vue
+++ components/iw-dialog/add-inwehub.vue
@@ -1,42 +1,45 @@
 <template>
 	<view>
-			<uni-popup :show="show" position="middle" mode="fixed" @hidePopup="hidePopup()">
-				<view class="component-addWeChat">
-					<view class="weChatText">截图保存后扫描 添加客服小哈微信</view>
-					<view class="weChatImg"><image src="../../static/images/xhWeChat@3x.png"></image></view>
-					<view class="bottom-btn" @click="clickItem">{{buttonText}}</view>
+		<uni-popup :show="show" position="middle" mode="fixed" @hidePopup="hidePopup()">
+			<view class="component-addWeChat">
+				<view class="close" @click="clickItem">
+					<text class="iconfont icon-guanbi"></text>
 				</view>
-			</uni-popup>
+				<view class="weChatText">截图保存后扫描 添加客服小哈微信</view>
+				<view class="weChatImg">
+					<image class="codeImg" src="../../static/images/xhWeChat@3x.png"></image>
+				</view>
+			</view>
+		</uni-popup>
 	</view>
 </template>
 <script>
-  import uniPopup from "@/components/uni-popup/uni-popup.vue"
+	import uniPopup from "@/components/uni-popup/uni-popup.vue"
 
-  export default {
-    components: {
-      uniPopup
-    },
-    data() {
-      return {
-        show: this.showPopup,
-				buttonText: '确定'
-      }
-    },
+	export default {
+		components: {
+			uniPopup
+		},
+		data() {
+			return {
+				show: this.showPopup
+			}
+		},
 		watch: {
 			showPopup(newValue, oldValue) {
 				this.show = newValue
 			}
 		},
-    props: {
+		props: {
 			showPopup: {
-        type: Boolean,
-        default: false
-      }
-    },
-    onLoad() {
+				type: Boolean,
+				default: false
+			}
+		},
+		onLoad() {
 
-    },
-    methods: {
+		},
+		methods: {
 			showDialog() {
 				this.show = true
 			},
@@ -52,40 +55,41 @@
 					return true
 				}
 			}
-    }
-  }
+		}
+	}
 </script>
 
 <style lang="less">
 	.component-addWeChat {
-    width: 600upx;
-    background: #FFFFFF;
-  }
-  .component-addWeChat .close {
-    color: #808080;
-    text-align: right;
-    padding-top: 30upx;
-    padding-right: 30upx;
-  }
-  .component-addWeChat .close .icon {
-    font-size: 37.96upx;
-  }
-  .component-addWeChat .weChatText {
-    color: #808080;
-    font-size: 25.96upx;
-    line-height: 36upx;
-    text-align: center;
-  }
-  .component-addWeChat .weChatImg {
-    width: 300upx;
-    height: 300upx;
-    margin: 25.96upx auto 0;
-  }
-  .component-addWeChat .weChatImg uni-image {
-    width: 100%;
-    height: 100%;
-  }
-
+		width: 600upx;
+		padding-bottom: 60upx;
+		border-radius: 26upx;
+		background: #FFFFFF;
+		.close {
+			color: #808080;
+			text-align: right;
+			padding-top: 30upx;
+			padding-right: 30upx;
+			.iconfont {
+				font-size: 37.96upx;
+			}
+		}
+		.weChatText {
+			color: #808080;
+			font-size: 25.96upx;
+			line-height: 36upx;
+			text-align: center;
+		}
+		.weChatImg {
+			width: 300upx;
+			height: 300upx;
+			margin: 25.96upx auto 0;
+			.codeImg {
+				width: 100%;
+				height: 100%;
+			}
+		}
+	}
 	.component-addWeChat .bottom-btn {
 		color: #03aef9;
 		height: 90upx;
