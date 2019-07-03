d9ec8af4890ac33af56ad91386f53df76917c644
edwin <edwincoder@163.com>
2019-07-03 12:46:08 +0800

    update 发布点评页图片样式

--- pages/dianping/add.vue
+++ pages/dianping/add.vue
@@ -13,7 +13,7 @@
       <textarea class="textarea" maxlength="-1" v-model="html" :placeholder="descPlaceholder" />
 
       <view class="container-images">
-        <view :key="image" v-for="(image, index) in images" class="container-image">
+        <view v-for="(image, index) in images" class="container-image">
           <text class="iconfont icon-times1 " @tap.stop.prevent="delImg(index)"/>
           <image :id="'image_' + index" mode="aspectFill" class="image" :src="image.path" />
         </view><view v-if="images.length < maxImageCount" class="component-photograph" @tap.stop.prevent="uploadImage()"><text class="iconfont icon-xiangji1 " /></view>
@@ -509,6 +509,11 @@ export default {
         right: -10upx;
         top: -26upx;
         z-index:9;
+    }
+
+    .container-images .container-image {
+      width: 122upx;
+      height: 122upx;
     }
 </style>
 <style>
